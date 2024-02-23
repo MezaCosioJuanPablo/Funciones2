67ad07fd8a79bc0bcc8274b272f64374cc61c337
MezaCosioJuanPablo <146865990+MezaCosioJuanPablo@users.noreply.github.com>
2024-02-23 13:06:44 -0600

    Add files via upload

---
+++ Funciones_2/Expresiones.js
@@ -0,0 +1,9 @@
+function saludar() {
+  const nombre = "unid";
+  console.log("Hola", nombre);
+}
+
+function saludarDos() {
+  const nombreDos = prompt("Ingresa un nombre");
+  console.log("Hola", nombreDos);
+}
